75d88522d7579f70f741ebf329088774edf14c83
Marcos José <70660098+MeloMarcoss@users.noreply.github.com>
2021-07-08 13:08:10 -0300

    Update Usuario.java

--- UFRPEIPIIReturn/project/src/model/Usuario.java
+++ UFRPEIPIIReturn/project/src/model/Usuario.java
@@ -1,5 +1,51 @@
 package model;
 
 public abstract class Usuario {
+  	private String nome,senha;
+	private String codigo;
+	
+	public Usuario(String nome, String senha, String codigo) {
+		this.nome =nome;
+		this.senha = senha;
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Nome: %s  Codigo: %s", this.getNome(), this.getCodigo());
+	}
+
 
 }
